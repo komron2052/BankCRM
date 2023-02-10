f446b9c7c190dbf799a5e47eefa489c0dde4bd1a
Safarmurod7 <92220640+Safarmurod7@users.noreply.github.com>
2023-02-10 15:49:28 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using BankCRM.IUserInterface;
+using System;
+namespace BankCRM
+{
+    internal class Bank
+    {
+        internal static void Main(string[] args)
+        {
+            MainInterface mainInterface = new MainInterface();
+            mainInterface.Demonstrate();
+        }
+    }
+}
